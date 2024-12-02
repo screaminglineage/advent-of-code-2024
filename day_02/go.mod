2c4d7e4dabc03d1e5679ac6607ea45f5e8f44ede
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-12-02 18:32:53 +0530

    finished part 1

---
+++ day_02/go.mod
@@ -0,0 +1,3 @@
+module day_02
+
+go 1.23.3
---
+++ day_02/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+    "strings"
+    "strconv"
+    "fmt"
+	"os"
+)
+
+const data_file = "data.txt"
+const test_file = "test.txt"
+
+func abs(num int) int {
+    if num < 0 {
+        return -num;
+    }
+    return num
+}
+
+func part_1(input string) int {
+    safe_seqs := 0
+    lines := strings.Split(input, "\n")
+    lines = lines[:len(lines)-1]
+
+    for _, line := range lines {
+        nums := strings.Split(line, " ")
+        safe := true
+        increasing := false
+        for i, _ := range nums {
+            if (i == len(nums) - 1) {
+                break
+            }
+
+            current, err := strconv.Atoi(nums[i])
+            if (err != nil) {
+                break
+            }
+            next, err := strconv.Atoi(nums[i+1])
+            if (err != nil) {
+                break
+            }
+
+            diff := current - next
+            if diff > 0 && increasing || diff > 0 && i == 0 {
+                increasing = true
+            } else if diff < 0 && !increasing || diff < 0 && i == 0 {
+                increasing = false
+            } else {
+                safe = false
+                break
+            }
+            diff = abs(diff)
+            if diff < 1 || diff > 3 {
+                safe = false
+                break
+            }
+        }
+        if safe {
+            safe_seqs += 1
+        }
+    }
+
+    return safe_seqs
+}
+
+func part_2() int {
+    return 0;
+}
+
+func main() {
+	data, err := os.ReadFile(data_file)
+	if err != nil {
+		log.Fatal(err)
+	}
+    input := string(data)
+    fmt.Println("Part 1: ", part_1(input))
+    fmt.Println("Part 2: ", part_2())
+}
+
+
