b6aa9cd1ece21a8cf69ed7abe064e27a94b4cba3
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-12-02 20:49:16 +0530

    finished part 2

--- day_02/main.go
+++ day_02/main.go
@@ -63,6 +63,39 @@ func part_1(input string) int {
     return safe_seqs
 }
 
+func is_safe(nums []string) bool {
+    increasing := false
+    for i, _ := range nums {
+        if (i == len(nums) - 1) {
+            break
+        }
+
+        current, err := strconv.Atoi(nums[i])
+        if (err != nil) {
+            break
+        }
+        next, err := strconv.Atoi(nums[i+1])
+        if (err != nil) {
+            break
+        }
+
+        diff := current - next
+        if diff > 0 && increasing || diff > 0 && i == 0 {
+            increasing = true
+        } else if diff < 0 && !increasing || diff < 0 && i == 0 {
+            increasing = false
+        } else {
+            return false
+        }
+        diff = abs(diff)
+        if diff < 1 || diff > 3 {
+            return false
+        }
+    }
+    return true
+}
+
+
 func part_2(input string) int {
     safe_seqs := 0
     lines := strings.Split(input, "\n")
@@ -70,48 +103,19 @@ func part_2(input string) int {
 
     for _, line := range lines {
         nums := strings.Split(line, " ")
-        safe := true
-        i := 0
-        j := 1
-        current, err := strconv.Atoi(nums[i])
-        if (err != nil) {
-            panic("Unreachable")
-        }
-        next, err := strconv.Atoi(nums[j])
-        if (err != nil) {
-            panic("Unreachable")
-        }
-
-        increasing := (current - next) < 0
-
-        for {
-            diff := abs(current - next)
-            if diff < 1 || diff > 3 {
-                safe = false
-                break
-            }
-            prev_increasing := increasing
-            if j >= len(nums) - 1 {
-                break
-            }
-            i += 1
-            j += 1
-            current, err = strconv.Atoi(nums[i])
-            if (err != nil) {
-                panic("Unreachable")
-            }
-            next, err = strconv.Atoi(nums[j])
-            if (err != nil) {
-                panic("Unreachable")
-            }
-            increasing = (current - next) < 0
-            if prev_increasing != increasing {
-                safe = false
-                break
-            }
-        }
-        if (safe) {
+        if is_safe(nums) {
+            fmt.Println(nums)
             safe_seqs += 1
+            continue
+        }
+        for i := range len(nums) {
+            sliced := append([]string{}, nums[:i]...)
+            sliced = append(sliced, nums[i+1:]...)
+            if is_safe(sliced) {
+                fmt.Println(nums)
+                safe_seqs += 1
+                break
+            }
         }
     }
     return safe_seqs
