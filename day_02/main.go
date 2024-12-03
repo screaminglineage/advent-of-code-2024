725a94199d9f1b6d385fdd241804ddcc041fffcc
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-12-03 11:56:12 +0530

    refactored part 1 to use is_safe

--- day_02/main.go
+++ day_02/main.go
@@ -18,51 +18,6 @@ func abs(num int) int {
     return num
 }
 
-func part_1(input string) int {
-    safe_seqs := 0
-    lines := strings.Split(input, "\n")
-    lines = lines[:len(lines)-1]
-
-    for _, line := range lines {
-        nums := strings.Split(line, " ")
-        safe := true
-        increasing := false
-        for i, _ := range nums {
-            if (i == len(nums) - 1) {
-                break
-            }
-
-            current, err := strconv.Atoi(nums[i])
-            if (err != nil) {
-                break
-            }
-            next, err := strconv.Atoi(nums[i+1])
-            if (err != nil) {
-                break
-            }
-
-            diff := current - next
-            if diff > 0 && increasing || diff > 0 && i == 0 {
-                increasing = true
-            } else if diff < 0 && !increasing || diff < 0 && i == 0 {
-                increasing = false
-            } else {
-                safe = false
-                break
-            }
-            diff = abs(diff)
-            if diff < 1 || diff > 3 {
-                safe = false
-                break
-            }
-        }
-        if safe {
-            safe_seqs += 1
-        }
-    }
-    return safe_seqs
-}
-
 func is_safe(nums []string) bool {
     increasing := false
     for i, _ := range nums {
@@ -94,6 +49,21 @@ func is_safe(nums []string) bool {
     }
     return true
 }
+
+func part_1(input string) int {
+    safe_seqs := 0
+    lines := strings.Split(input, "\n")
+    lines = lines[:len(lines)-1]
+
+    for _, line := range lines {
+        nums := strings.Split(line, " ")
+        if is_safe(nums) {
+            safe_seqs += 1
+        }
+    }
+    return safe_seqs
+}
+
 
 
 func part_2(input string) int {
