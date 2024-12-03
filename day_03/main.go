5f10fdb7d7a0b756aafcbd9badd1b6a5e3df31d1
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-12-03 13:08:31 +0530

    finished part 2

--- day_03/main.go
+++ day_03/main.go
@@ -11,6 +11,16 @@ import (
 
 const data_file = "data.txt"
 const test_file = "test.txt"
+const test_file_part2 = "test_2.txt"
+
+func eval_mul(expr string) int {
+    nums, _ := strings.CutPrefix(expr, "mul(")
+    nums2, _ := strings.CutSuffix(nums, ")")
+    nums3 := strings.Split(nums2, ",")
+    a, _ := strconv.Atoi(nums3[0])
+    b, _ := strconv.Atoi(nums3[1])
+    return a * b
+}
 
 func part_1(input string) int {
     mul_regex := regexp.MustCompile(`mul\(\d+,\d+\)`)
@@ -31,7 +41,46 @@ func part_1(input string) int {
 }
 
 func part_2(input string) int {
-    return 0;
+    mul_regex := regexp.MustCompile(`mul\(\d+,\d+\)`)
+    do_regex := regexp.MustCompile(`do(n't)?\(\)`)
+
+    sum := 0
+    bounds := mul_regex.FindStringIndex(input)
+    if (bounds == nil) {
+        log.Fatal("Unreachable: regex is incorrect")
+    }
+    sum += eval_mul(input[bounds[0]:bounds[1]])
+    input = input[bounds[1]:]
+
+    do := true
+    for len(input) > 0 {
+        mul_bounds := mul_regex.FindStringIndex(input)
+        if (mul_bounds == nil) {
+            break
+        }
+
+        do_bounds := do_regex.FindStringIndex(input[:mul_bounds[0]])
+        if do_bounds == nil {
+            if do {
+                fmt.Println(input[mul_bounds[0]:mul_bounds[1]])
+                sum += eval_mul(input[mul_bounds[0]:mul_bounds[1]])
+            }
+            input = input[mul_bounds[1]:]
+            continue
+        }
+        res := input[do_bounds[0]:do_bounds[1]]
+        if res == "don't()" {
+            do = false
+        } else {
+            do = true
+        }
+        if do {
+            fmt.Println(input[mul_bounds[0]:mul_bounds[1]])
+            sum += eval_mul(input[mul_bounds[0]:mul_bounds[1]])
+        }
+        input = input[mul_bounds[1]:]
+    }
+    return sum;
 }
 
 func main() {
